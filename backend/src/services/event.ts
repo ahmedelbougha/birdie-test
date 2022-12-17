94b68e42be5f84d7066b4dfc9db53d0f8bee4298
ahmed.elbougha <ahmed.elbougha@gmail.com>
2022-12-17 08:22:33 +0200

    adding recipients with integration tests

--- backend/src/services/event.ts
+++ backend/src/services/event.ts
@@ -1,4 +1,10 @@
+import { Sequelize } from 'sequelize';
 import Event from '../models/event';
+
+type recipientSummary = {
+  event_type: string;
+  event_type_count: number;
+};
 
 /**
  * Fetching array of events with their counts
@@ -22,4 +28,57 @@ const fetchEvents = async (
   return { events: rows, count };
 };
 
-export default { fetchEvents };
+/**
+ * Fetching array of recipient with their counts
+ *
+ * @param {number} offset
+ * @param {number} limit
+ */
+const fetchRecipients = async (
+  offset = 0,
+  limit = 100
+): Promise<{ recipients: Event[] }> => {
+  const rows = await Event.findAll({
+    attributes: [
+      [
+        Sequelize.fn('DISTINCT', Sequelize.col('care_recipient_id')),
+        'care_recipient_id',
+      ],
+    ],
+    offset,
+    limit,
+  });
+  return { recipients: rows };
+};
+
+/**
+ * Fetching summary of recipient's events with their counts
+ *
+ * @param {string} recipientId
+ */
+const fetchRecipientSummary = async (
+  recipientId: string
+): Promise<{
+  care_recipient_id: string;
+  recipient_summary: recipientSummary[];
+}> => {
+  const rows = await Event.findAll({
+    attributes: [
+      'event_type',
+      [Sequelize.fn('Count', Sequelize.col('event_type')), 'event_type_count'],
+    ],
+    group: ['care_recipient_id', 'event_type'],
+    having: { care_recipient_id: recipientId },
+  });
+
+  const recipientSummary = <recipientSummary[]>(
+    (<unknown>rows.map((row) => ({ ...row.dataValues })))
+  );
+
+  return {
+    care_recipient_id: recipientId,
+    recipient_summary: recipientSummary,
+  };
+};
+
+export default { fetchEvents, fetchRecipients, fetchRecipientSummary };
