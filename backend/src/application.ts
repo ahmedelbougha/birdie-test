81f39f7bf37a3336d0d0b0347a92e5cd7321288c
ahmed.elbougha <ahmed.elbougha@gmail.com>
2022-12-17 08:18:54 +0200

    adding events to application.ts

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,14 +1,15 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import 'dotenv/config';
-
 import { pingController } from './controllers/ping';
 import errorController from './controllers/error';
+import eventRoutes from './routes/event';
 
 const app = express();
 
 app.use(pingController);
 app.use(cors());
+app.use('/events', eventRoutes);
 app.use(errorController.notFound);
 
 export default app;
