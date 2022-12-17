ff70e6f44b5b97d14365e14c26c6a92b6fab3216
ahmed.elbougha <ahmed.elbougha@gmail.com>
2022-12-17 08:12:03 +0200

    Adding error controller with integration tests

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,8 +1,14 @@
-import * as express from "express";
-import {pingController} from "./controllers/ping";
+import * as express from 'express';
+import * as cors from 'cors';
+import 'dotenv/config';
+
+import { pingController } from './controllers/ping';
+import errorController from './controllers/error';
 
 const app = express();
 
 app.use(pingController);
+app.use(cors());
+app.use(errorController.notFound);
 
 export default app;
