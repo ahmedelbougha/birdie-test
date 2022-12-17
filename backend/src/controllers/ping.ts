f8da89d76a4d9f920d1529dc7fc41c0c4458ca76
ahmed.elbougha <ahmed.elbougha@gmail.com>
2022-12-17 08:27:06 +0200

    Adding final integration tests and final paths to ping controller

--- backend/src/controllers/ping.ts
+++ backend/src/controllers/ping.ts
@@ -1,9 +1,10 @@
-import * as express from "express";
+import * as express from 'express';
 
 export const pingController = express.Router();
 
-pingController.get('/hello', (_, res) => {
+pingController.get('/', (_, res) => {
   res.status(200).json({
-    greetings: 'Thank you for spending some time on this test. All the best 🙌'
+    greetings: 'Thank you for spending some time on this test. All the best 🙌',
+    routes: '/events, /events/[care-recipient-id], /recipients, and /recipients/[care-recipient-id]',
   });
 });
