83042ff0f66628996232f6c9ee8fe6cf8e04a45b
ahmed.elbougha <ahmed.elbougha@gmail.com>
2022-12-19 01:04:52 +0200

    Adding pages with their tests and snapshots

---
+++ frontend/src/pages/Error.tsx
@@ -0,0 +1,24 @@
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import { StyledError } from '../components/styles/Error.styled';
+import { Error } from '../store/reducers/general.d';
+
+export default function ErrorPage({
+  error = {},
+  errorStatus,
+}: Error): JSX.Element {
+  let statusText = '';
+  if (errorStatus) {
+    // statusText = error.statusText;
+  }
+  return (
+    <StyledError>
+      <div>
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{statusText}</i>
+        </p>
+      </div>
+    </StyledError>
+  );
+}
